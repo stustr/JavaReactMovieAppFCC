b693229665bb3a56798f6583fcad8266711a04cd
Struan <60407763+stustr@users.noreply.github.com>
2023-04-05 11:09:33 +0100

    useState, useEffect implemented for axios requests

--- viteMoviesJavaReact/src/App.jsx
+++ viteMoviesJavaReact/src/App.jsx
@@ -1,14 +1,32 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import api from "./api/axiosConfig";
 
 function App() {
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setMovies(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+  
 
   return (
     <div className="App">
-    <div>
-      <p>Hello World</p>
+      <div>
+        <p>Hello World</p>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
