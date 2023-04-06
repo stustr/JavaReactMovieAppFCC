040c4de9283a02cd04700eab93b34b0b8b6ade8c
Struan <60407763+stustr@users.noreply.github.com>
2023-04-06 20:14:33 +0100

    Review route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,29 @@ import "./App.css";
 import api from "./api/axiosConfig";
 import Layout from "./components/Layout";
 import Home from "./components/home/Home";
+import Reviews from "./components/reviews/Reviews";
 
 function App() {
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
 
   const getMovies = async () => {
     try {
       const response = await api.get("/api/v1/movies");
       setMovies(response.data);
       console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviews);
     } catch (error) {
       console.error(error);
     }
@@ -29,6 +43,16 @@ function App() {
           <Route
             path="/"
             element={movies ? <Home movies={movies} /> : <p>Loading...</p>}
+          />
+          <Route
+            path="/Reviews/:movieId"
+            element={
+              <Reviews
+                getMovieData={getMovieData}
+                reviews={reviews}
+                setReviews={setReviews}
+              />
+            }
           />
         </Route>
       </Routes>
