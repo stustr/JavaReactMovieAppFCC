860de23b64ba766829d9a617d91e1510a9e2c226
Struan <60407763+stustr@users.noreply.github.com>
2023-04-06 19:21:06 +0100

    useState and useEffect added to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,37 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import api from "./api/axiosConfig";
+import { useEffect } from "react";
+import Layout from "./components/Layout";
+import Home from "./components/home/Home";
 
 function App() {
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      setMovies(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
 
   return (
     <div className="App">
-    <div>
-      <p>Hello World</p>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+        </Route>
+      </Routes>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
