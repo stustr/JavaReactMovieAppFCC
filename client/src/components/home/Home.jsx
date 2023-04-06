fcb9c9907958087123c1d1bee639b8829bdf0b7a
Struan <60407763+stustr@users.noreply.github.com>
2023-04-06 19:21:24 +0100

    Hero component added to Home

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,4 +1,6 @@
-const Home = ({movies}) => {
-  return <div>Welcome</div>;
+import Hero from "../hero/Hero";
+
+const Home = ({ movies }) => {
+  return <Hero movies={movies} />;
 };
 export default Home;
