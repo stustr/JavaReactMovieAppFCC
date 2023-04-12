92a27877012b336451f9aab279056410870d525a
Struan <60407763+stustr@users.noreply.github.com>
2023-04-12 09:55:12 +0100

    💄 styling at main level

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router}>
-      <App />
+      <App className="flex-col align-middle justify-center"/>
     </RouterProvider>
   </React.StrictMode>
 );
