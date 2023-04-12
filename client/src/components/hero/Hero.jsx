f49d66fcc3b52acdec3afce9c5d086623bb205b1
Struan <60407763+stustr@users.noreply.github.com>
2023-04-12 09:54:10 +0100

    useNavigate from react-router-dom utilised

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,7 +1,14 @@
 import Carousel from "react-material-ui-carousel";
 import { Paper } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Hero = ({ movies }) => {
+  const navigate = useNavigate();
+
+  function reviews(movieId) {
+    navigate(`/Reviews/${movieId}`);
+  }
+
   return (
     <div>
       <Carousel>
@@ -15,6 +22,12 @@ const Hero = ({ movies }) => {
                 <div>
                   <h4>{movie.title} </h4>
                 </div>
+                <button
+                  className="flex justify-end bg-slate-500 p-3 rounded-md"
+                  onClick={() => reviews(movie.imdbId)}
+                >
+                  Reviews
+                </button>
               </div>
             </Paper>
           );
